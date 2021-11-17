7e036fad677d4cdaa3d3acdd6259ef52c2ab0a26
Ashley Fellows <ashley.r.fellows@gmail.com>
2021-11-16 21:21:30 -0500

    started userprofile class

---
+++ BlueBadgeProject.Data/UserProfiles.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBadgeProject.Data
+{
+    public class UserProfile
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+
+        public virtual Recommendation Recs { get; set; }
+
+        public virtual Group UGroups { get; set; }
+
+
+        //private List<Groups> _groups = new List<Groups>();
+        //private List<Recommendations> _recommendations = new List<Recommendations>();
+
+        //public List<Groups> Groups
+        //{
+        //    get
+        //    { return _groups; }
+        //}
+
+        //public List<Recommendations> Recommendations
+        //{
+        //    get 
+        //    { return _recommendations; }
+        //}
+    }
+}
