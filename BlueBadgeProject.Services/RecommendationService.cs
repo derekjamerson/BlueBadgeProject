1e6a2c4bc035b6614479906977a29e1d3ef075cc
Derek Jamerson <derekjamerson@gmail.com>
2021-11-17 14:01:19 -0500

    done

--- BlueBadgeProject.Services/RecommendationService.cs
+++ BlueBadgeProject.Services/RecommendationService.cs
@@ -25,5 +25,63 @@ namespace BlueBadgeProject.Services
                 return ctx.SaveChanges() == 1;
             }
         }
+        public RecItem GetRecById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Recommendations
+                        .Single(e => e.RecommendationId == id);
+                return
+                    new RecItem
+                    {
+                        RecommendationId = entity.RecommendationId,
+                        UserProfileId = entity.UserProfileId,
+                        GroupId = entity.GroupId,
+                        SongId = entity.SongId
+                    };
+            }
+        }
+        public IEnumerable<RecItem> GetRecsByUserProfileId(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Recommendations
+                        .Where(e => e.UserProfileId == id)
+                        .Select(
+                            e =>
+                                new RecItem
+                                {
+                                    RecommendationId = e.RecommendationId,
+                                    UserProfileId = e.UserProfileId,
+                                    SongId = e.SongId,
+                                    GroupId = e.GroupId
+                                });
+                return query.ToArray();
+            }
+        }
+        public IEnumerable<RecItem> GetRecsByGroupId(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Recommendations
+                        .Where(e => e.GroupId == id)
+                        .Select(
+                            e =>
+                                new RecItem
+                                {
+                                    RecommendationId = e.RecommendationId,
+                                    UserProfileId = e.UserProfileId,
+                                    SongId = e.SongId,
+                                    GroupId = e.GroupId
+                                });
+                return query.ToArray();
+            }
+        }
     }
 }
