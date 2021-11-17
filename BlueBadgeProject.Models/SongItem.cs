4ceef14fa50fbb6257659e8b8789cf6809c5c99d
Derek Jamerson <derekjamerson@gmail.com>
2021-11-17 12:00:22 -0500

    Song CRUD

---
+++ BlueBadgeProject.Models/SongItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBadgeProject.Models
+{
+    public class SongItem
+    {
+        public int SongId { get; set; }
+        public string Title{ get; set; }
+        public string Artist { get; set; }
+    }
+}
---
+++ BlueBadgeProject.Services/SongService.cs
@@ -0,0 +1,78 @@
+﻿using BlueBadgeProject.Data;
+using BlueBadgeProject.Models;
+using ElevenNote.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBadgeProject.Services
+{
+    public class SongService
+    {
+        public bool CreateSong(SongCreate model)
+        {
+            var entity = new Song()
+            {
+                Title = model.Title,
+                Artist = model.Artist,
+            };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Songs.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public IEnumerable<SongItem> GetSongs()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Songs
+                        .Select(
+                            e =>
+                                new SongItem
+                                {
+                                    SongId = e.SongId,
+                                    Title = e.Title,
+                                    Artist = e.Artist
+                                });
+                return query.ToArray();
+            }
+        }
+        public SongItem GetSongById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Songs
+                        .Single(e => e.SongId == id);
+                return
+                    new SongItem
+                    {
+                        SongId = entity.SongId,
+                        Title = entity.Title,
+                        Artist = entity.Artist
+                    };
+            }
+        }
+        public bool UpdateSong(SongItem model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Songs
+                        .Single(e => e.SongId == model.SongId);
+
+                entity.Title = model.Title;
+                entity.Artist = model.Artist;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
