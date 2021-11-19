01141aa4264a252223173a24c0985c97332570ea
Johnson Lee <johnson.hwang1008@gmail.com>
2021-11-18 20:19:37 -0500

    ad

---
+++ BlueBadgeProject.Data/Group.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBadgeProject.Data
+{
+    public class Group
+    {
+        [Key]
+        public int GroupId { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<UserProfile> ListOfUsers { get; set; }
+        
+
+    }
+}
