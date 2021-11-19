51c841773dc59f98abf9a14e419ab7543f4c3c32
Derek Jamerson <derekjamerson@gmail.com>
2021-11-18 21:20:59 -0500

    done

---
+++ BlueBadgeProject.WebAPI/Controllers/SongController.cs
@@ -0,0 +1,35 @@
+﻿using BlueBadgeProject.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using BlueBadgeProject.Models;
+
+namespace BlueBadgeProject.WebAPI.Controllers
+{
+    [Authorize]
+    public class SongController : ApiController
+    {
+        private SongService CreateSongService()
+        {
+            var userId = User.Identity.GetUserId();
+            var songService = new SongService(userId);
+            return songService;
+        }
+
+        public IHttpActionResult Post(SongCreate song)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateSongService();
+
+            if (!service.CreateSong(song))
+                return InternalServerError();
+
+            return Ok($"");
+        }
+    }
+}
