6e107500111a29bf7528e3171e0c6e99fd026907
Ashley Fellows <ashley.r.fellows@gmail.com>
2021-11-18 20:57:20 -0500

    added GetUserProfiles()

--- BlueBadgeProject.Services/UserProfileService.cs
+++ BlueBadgeProject.Services/UserProfileService.cs
@@ -10,19 +10,45 @@ namespace BlueBadgeProject.Services
 {
     public class UserProfileService
     {
+        private readonly string _userId;
+        public UserProfileService(string userId)
+        {
+            _userId = userId;
+        }
         public bool CreateUserProfile(UserProfileCreate model)
         {
             var entity =
                 new UserProfile()
                 {
+                    UserId = _userId,
                     FirstName = model.FirstName,
                     LastName = model.LastName
                 };
-            
+
             using (var ctx = new ApplicationDbContext())
             {
                 ctx.UserProfiles.Add(entity);
                 return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<UserProfileItem> GetUserProfiles()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .UserProfiles
+                        .Select(
+                            e =>
+                                new UserProfileItem
+                                {
+                                    FirstName = e.FirstName,
+                                    LastName = e.LastName
+                                }
+                         );
+
+                return query.ToArray();
             }
         }
     }
