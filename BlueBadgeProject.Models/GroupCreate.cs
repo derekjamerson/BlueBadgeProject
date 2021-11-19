83981f43575e8fde56a8ece6cb20dd9d2c81cb19
Johnson Lee <johnson.hwang1008@gmail.com>
2021-11-18 20:39:09 -0500

    asdf

---
+++ BlueBadgeProject.Models/GroupCreate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBadgeProject.Models
+{
+    public class GroupCreate
+    {
+        [Required]
+        public int GroupId { get; set; }
+        [Required]
+        public int UserProfileId { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
+
