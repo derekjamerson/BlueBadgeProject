ab9b7cee98b4022faff677b1bdce680a8703f214
Ashley Fellows <ashley.r.fellows@gmail.com>
2021-11-18 19:04:11 -0500

    I hope this is finally correct!

--- BlueBadgeProject.Data/UserProfiles.cs
+++ BlueBadgeProject.Data/UserProfiles.cs
@@ -4,13 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlueBadgeProject.Data
 {
     public class UserProfile
     {
-        [Key]
-        public int UserId { get; set; }
+        [ForeignKey("ApplicationUser")]
+        public string UserId { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
 
         public string FirstName { get; set; }
 
